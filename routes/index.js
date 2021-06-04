3794c499ec45efd5bced6e2fa77f7f92c1590a57
reqhiem <jperca@unsa.edu.pe>
2021-06-04 11:32:34 -0500

    Staging more content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Inicio' });
+  res.render('index', { title: 'Pochos\' Com' });
 });
 
 module.exports = router;

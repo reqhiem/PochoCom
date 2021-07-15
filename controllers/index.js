172507b2a507dbff950b6bb0d0b98166d33a3029
reqhiem <jperca@unsa.edu.pe>
2021-07-15 09:17:00 -0500

    Guardando mejoras en los controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+// For test connection to database
+const {Sequelize} = require('sequelize')
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('pochoscom', 'root', 'admin', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-    res.render('index', {title: "Inicio", page: "inicio"});
+    let msg;
+    try {
+        sequelize.authenticate();
+        msg = 'Connection has been established successfully.';
+    } catch (error) {
+        msg = 'Unable to connect to the database:';
+    }
+    res.render('index', {title: "Inicio", page: "inicio", msg: msg});
 });
 
 module.exports = router;
